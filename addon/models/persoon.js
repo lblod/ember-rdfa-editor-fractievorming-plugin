9049605dd51f007dabc7299c130639d3e35d8e03
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-10-30 20:20:12 +0100

    extend rdfa props of person and mandataat
    so it can be used on reload

--- addon/models/persoon.js
+++ addon/models/persoon.js
@@ -15,5 +15,15 @@ export default Model.extend({
   geslacht: belongsTo('geslacht-code', { inverse: null }),
   isAangesteldAls: hasMany('mandataris', { inverse: 'isBestuurlijkeAliasVan' }),
   isKandidaatVoor: hasMany('kandidatenlijst', { inverse: 'kandidaten' }),
-  verkiezingsresultaten: hasMany('verkiezingsresultaat', { inverse: null})
+  verkiezingsresultaten: hasMany('verkiezingsresultaat', { inverse: null}),
+
+  rdfaBindings: { // eslint-disable-line ember/avoid-leaking-state-in-ember-objects
+    class: "http://www.w3.org/ns/person#Person",
+    achternaam: "http://xmlns.com/foaf/0.1/familyName",
+    gebruikteVoornaam: "http://data.vlaanderen.be/ns/persoon#gebruikteVoornaam",
+    alternatieveNaam: "http://xmlns.com/foaf/0.1/name",
+    geslacht: "http://data.vlaanderen.be/ns/persoon#geslacht",
+    isAangesteldAls: "http://data.vlaanderen.be/ns/mandaat#isAangesteldAls",
+    geboorte: "http://data.vlaanderen.be/ns/persoon#heeftGeboorte"
+  }
 });
