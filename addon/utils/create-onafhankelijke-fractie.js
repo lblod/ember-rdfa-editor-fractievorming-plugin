54c89e2266694f8b5b3db5790d1f8f2600f1e832
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-11-02 09:43:06 +0100

    remove obsolete files

--- addon/utils/create-onafhankelijke-fractie.js
+++
@@ -1,8 +0,0 @@
-import Fractie from '../models/fractie';
-import Fractietype from '../models/fractietype';
-export default function createOnafhankelijkeFractie(bestuursorganenInTijd = []) {
-    return Fractie.create({
-      naam: 'Onafhankelijk',
-      bestuursOrganenInTijd: bestuursorganenInTijd,
-      fractietype: Fractietype.create({type: 'Onafhankelijk'})});
-}
--- app/models/aan-te-stellen-mandataris.js
+++
@@ -1,1 +0,0 @@
-export { default } from '@lblod/ember-rdfa-editor-fractievorming-plugin/models/mandataris';
