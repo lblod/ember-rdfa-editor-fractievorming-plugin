b94152c57562e1e956aa4c864556714150e1519e
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-12-14 10:55:57 +0100

    update bug

--- addon/components/editor-plugins/fractievorming-card.js
+++ addon/components/editor-plugins/fractievorming-card.js
@@ -223,7 +223,7 @@ export default Component.extend(SerializationHelper, {
     },
 
     toggleNieuweStijl(){
-      this.toggelProperty('nieuweStijl');
+      this.toggleProperty('nieuweStijl');
     }
   }
 });
