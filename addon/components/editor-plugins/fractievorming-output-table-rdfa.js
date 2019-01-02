10ffc8f3a69ac042573d3e757d9eef518e93ae27
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-01-02 12:08:57 +0100

    update sorting

--- addon/components/editor-plugins/fractievorming-output-table-rdfa.js
+++ addon/components/editor-plugins/fractievorming-output-table-rdfa.js
@@ -1,10 +1,17 @@
 import Component from '@ember/component';
 import layout from '../../templates/components/editor-plugins/fractievorming-output-table-rdfa';
 import { computed } from '@ember/object';
-
 export default Component.extend({
   layout,
   sortedMandatarissen: computed('mandatarissen.[]', function(){
-    return this.mandatarissen.sort((a,b) => a.isBestuurlijkeAliasVan.gebruikteVoornaam.trim().localeCompare(b.isBestuurlijkeAliasVan.gebruikteVoornaam.trim()));
-  })
+    return this.mandatarissen.sort((a,b) => this.sortFractie(a, b) || this.sortName(a, b));
+  }),
+
+  sortName: (a, b) => {
+    return a.isBestuurlijkeAliasVan.achternaam.trim().localeCompare(b.isBestuurlijkeAliasVan.achternaam.trim());
+  },
+
+  sortFractie: (a, b) => {
+    return a.heeftLidmaatschap.binnenFractie.naam.trim().localeCompare(b.heeftLidmaatschap.binnenFractie.naam.trim());
+  }
 });
